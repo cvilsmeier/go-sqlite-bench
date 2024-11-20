87ce8ac8cc414149967cd75c8b452dc5e2a3abef
Daniel Carbone <daniel.p.carbone@gmail.com>
2024-11-20 08:13:50 -0600

    adding glebarez command

--- run-all.sh
+++ run-all.sh
@@ -10,11 +10,12 @@ stat _build > /dev/null
 cd _build
 
 # run every benchmark twice
-date && ./bench-craw    bench.db  && ./bench-craw    bench.db
-date && ./bench-eaton   bench.db  && ./bench-eaton   bench.db
-date && ./bench-mattn   bench.db  && ./bench-mattn   bench.db
-date && ./bench-modernc bench.db  && ./bench-modernc bench.db
-date && ./bench-ncruces bench.db  && ./bench-ncruces bench.db
-date && ./bench-sqinn   bench.db  && ./bench-sqinn   bench.db
-date && ./bench-zombie  bench.db  && ./bench-zombie  bench.db
+date && ./bench-craw     bench.db  && ./bench-craw     bench.db
+date && ./bench-eaton    bench.db  && ./bench-eaton    bench.db
+date && ./bench-glebarez bench.db  && ./bench-glebarez bench.db
+date && ./bench-mattn    bench.db  && ./bench-mattn    bench.db
+date && ./bench-modernc  bench.db  && ./bench-modernc  bench.db
+date && ./bench-ncruces  bench.db  && ./bench-ncruces  bench.db
+date && ./bench-sqinn    bench.db  && ./bench-sqinn    bench.db
+date && ./bench-zombie   bench.db  && ./bench-zombie   bench.db
 date
